5e98b07975409539c96c8f23bcb3676cde22c485
Francesco <francesco12382@gmail.com>
2017-09-22 13:15:15 +0800

    submitted and finished application software

---
+++ applications.php
@@ -0,0 +1,267 @@
+<html>
+
+  <!-- header include -->
+
+  <!-- for webhost change to this format -->
+
+  <?php include 'includes/header.php'; ?>
+
+  <body>
+
+    <!-- Nav Include -->
+
+      <?php include 'includes/nav.php'; ?>
+
+
+<div class="text-center">
+
+  <h1>Application Software</h1>
+
+  <p>
+    application sofware is useful software that through the operating system allows you to complete tasks such as writing a letter or browsing the web.
+    Some of the options offered by the apllication software infact are from the operating system and can be found in almost all apllications.
+  </p>
+  <img class="img-fluid" src="https://mrkelly.000webhostapp.com/content/IB_Computer_Science/Topic_2_Computer_organization/images/software_common_features.PNG" alt="Image of top file options">
+</div>
+
+      <ul>
+        <li id="WordProcessing">Word Processing:
+          <ul>
+            <li>Defenition: This is application software that allows you to create documents and edit primarily text bsaed documents.</li>
+            <li>Uses:
+              <ul>
+                <li>Legal documents</li>
+                <li>Essays</li>
+                <li>reports</li>
+                <li>letters</li>
+                <li>Basically anything that primarily involves text and doesn't need to be interactive.</li>
+              </ul>
+            </li>
+            <li>Features:
+              <ul>
+                <li>Formatting options</li>
+                <li>SpellChacker</li>
+                <li>realistic demo(WYSISYG)</li>
+                <li>templates</li>
+                <li>mostly page formating and tpying utility tools</li>
+              </ul>
+            </li>
+            <li>Examples:
+              <ul>
+                <li>Microsoft Word
+                   <br>
+                <img class="img-fluid" src="https://mrkelly.000webhostapp.com/content/IB_Computer_Science/Topic_2_Computer_organization/images/word_proc_features.PNG" alt="Picture of microsoft word as an example."></li>
+                <li>Googles Documents</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="Emailclient">Email client:
+          <ul>
+            <li>Defenition:  This is application software that allows you to send emails to other people.</li>
+            <li>Uses:
+              <ul>
+                <li>Sending Legal documents</li>
+                <li>Sending Memos</li>
+                <li>Communication between offices</li>
+                <li>Sending letters/e-mails</li>
+                <li>Sending small files</li>
+                <li>Anything related to sending and e-mail.</li>
+              </ul>
+            </li>
+            <li>Features:
+              <ul>
+                <li>Formatting options for text (minimal)</li>
+                <li>SpellChacker</li>
+                <li>Responsive e-mails/templates</li>
+                <li>contacts list</li>
+                <li>personalized email adress</li>
+              </ul>
+            </li>
+            <li>Examples:
+              <ul>
+                <li>Outlook</li>
+                <li>Gmail</li>
+                <li>Yahoo mail</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="spreadSheet">SpreadSheet:
+          <ul>
+            <li>Defenition: This is application software that allows you to create spreadsheets which can hold text, numbers and formulas.</li>
+            <li>Uses:
+              <ul>
+                <li>Graphs</li>
+                <li>Number analysis</li>
+                <li>number crunching</li>
+                <li>Difficult math equations</li>
+                <li>Store almost unlimited numberof values on a sheet both strings and numbers(int/floats)</li>
+                <li>multiple sheets</li>
+              </ul>
+            </li>
+            <li>Features:
+              <ul>
+                <li>Custom formulas</li>
+                <li>Automatic formula adjustments</li>
+                <li>graphs made</li>
+                <li>Macro options</li>
+                <li>Limited formating / customization options</li>
+              </ul>
+            </li>
+            <li>Examples:
+              <ul>
+                <li>Microsoft spreadsheets</li>
+                <li>Google spreadsheets</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="DatabaseManagementSystem">Database Management System:
+          <ul>
+            <li>Defenition:  This is application software that allows you to connect to a database and see and edit its content. </li>
+            <li>Uses:
+              <ul>
+                <li>Database correction</li>
+                <li>Database analysis</li>
+                <li>Database accessing</li>
+                <li>Database Querys</li>
+                <li>Database Backups</li>
+                <li>Anything related to managing or maintaing a database.</li>
+              </ul>
+            </li>
+            <li>Features:
+              <ul>
+                <li>Query presets</li>
+                <li>Custom Query options</li>
+                <li>GUI of query results</li>
+                <li>Table options (Cell creation, table creation ect.)</li>
+              </ul>
+            </li>
+            <li>Examples:
+              <ul>
+                <li>FCS Analytics by FCS Computer Systems</li>
+                <li>QlikSense by Qlik</li>
+                <li>SPSS by IBM</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="Webbrowser">Web browser:
+            <ul>
+              <li>Defenition:  This is application software that allows you to access the world wide web and navigate it.</li>
+              <li>Uses:
+              <ul>
+                <li>Connecting to servers</li>
+                <li>Seeing websites</li>
+                <li>Sending emails</li>
+                <li>Playing online games</li>
+                <li>Watching Youtube</li>
+                <li>Anything related to using the features of the World wide web</li>
+              </ul>
+              </li>
+              <li>Features:
+              <ul>
+                <li>connects you to the world wide web</li>
+                <li>allows for communcation between you and anything on the world wide web</li>
+                <li>Search history</li>
+                <li>Saving site urls</li>
+              </ul>
+              </li>
+              <li>Examples:
+              <ul>
+                <li>Google Chrome</li>
+                <li>Internet Explorer</li>
+                <li>Microsoft Edge/li>
+              </ul>
+              </li>
+            </ul>
+        </li>
+        <li id="ComputerAidedDesign">Computer Aided Design:
+          <ul>
+            <li>Defenition:  This is application software that helps you design things in either 2D or 3D. .</li>
+            <li>Uses:
+            <ul>
+              <li>Architecture</li>
+              <li>2D design</li>
+              <li>3D modeling</li>
+            </ul>
+            </li>
+            <li>Features:
+            <ul>
+              <li>allows you to create accurate representations</li>
+              <li>gives an accurate estimates of real world sizes</li>
+              <li>Allows you to edit individual lengths or vertices</li>
+            </ul>
+            </li>
+            <li>Examples:
+            <ul>
+              <li>Autodesk</li>
+              <li>3D Slash</li>
+              <li>TinkerCad</li>
+            </ul>
+            </li>
+          </ul>
+         </li>
+        <li id="GraphicProcessingSoftware">Graphic Processing Software:
+          <ul>
+            <li>Defenition:  This is application software that allows you to manipulate and edit pictures.</li>
+            <li>Uses:
+            <ul>
+              <li>Poster Making</li>
+              <li>Image editing</li>
+              <li>Color correction</li>
+            </ul>
+            </li>
+            <li>Features:
+            <ul>
+              <li>allows you to edit individual pixels</li>
+              <li>gives an accurate estimates of real item</li>
+              <li>Allows you to add basic text and fives you basic formating options</li>
+            </ul>
+            </li>
+            <li>Examples:
+            <ul>
+              <li>Adobe Photoshop</li>
+              <li>Gimp</li>
+            </ul>
+            </li>
+          </ul>
+        </li>
+        <li id="Presentation software">Presentation software:
+          <ul>
+            <li>Defenition: This is applciation software that allows you to create slides with wich data, image or numbers can be put on and presented.</li>
+            <li>Uses:
+            <ul>
+              <li>Interactive presentations</li>
+              <li>Presentation</li>
+              <li>Interactive reports</li>
+            </ul>
+            </li>
+            <li>Features:
+            <ul>
+              <li>allows you to edit and add slides</li>
+              <li>allows for image and text formating options</li>
+              <li>allows you to personalise each slides and make templates</li>
+            </ul>
+            </li>
+            <li>Examples:
+            <ul>
+              <li>Microsoft powerpoint</li>
+              <li>Google slides</li>
+            </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+
+  </body>
+
+  <!-- JavaScript Include -->
+
+  <?php include 'includes/javaScriptCdn.php'; ?>
+
+
+
+</html>
